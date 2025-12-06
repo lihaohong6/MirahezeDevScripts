acd789e0207fafb73ac8f32ae68cfbf1055b0632
ccxtwf <theweirdfandom@gmail.com>
2025-12-07 00:37:52 +0700

    Build tools: Allow automatic setting of CDN entrypoint on compilation, auto-generate i18n loading logic on scripts ported from Fandoom

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,7 +1,9 @@
 import autogenerateEntrypoint from "./autogenerate-entrypoint.js";
 import createMwGadgetImplementation from "./create-mw-gadget-implementation.js";
+import fandoomUtilsI18nInjector from "./fandoom-utils-i18n-injector.js";
 
 export {
   autogenerateEntrypoint,
   createMwGadgetImplementation,
+  fandoomUtilsI18nInjector,
 };
